795d1577893d3ccba2a429764e12bd07d37aa874
Vincent Van Steenbergen <v.vansteenbergen@gmail.com>
2013-02-25 19:05:22 +0100

    Ajout et configuration des ressources (bootstrap, angular)  et framework de test JS

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,24 @@
 modules = {
+
     application {
-        resource url:'js/application.js'
+        dependsOn 'jquery, angularResource, bootstrap'
+        resource url: 'css/application.css'
+        resource url: 'js/angular/app/application.js'
     }
+
+    bootstrap {
+        dependsOn 'jquery'
+        resource url: 'js/bootstrap/css/bootstrap.min.css'
+        resource url: 'js/bootstrap/js/bootstrap.min.js'
+    }
+
+    angular {
+        resource id: 'js', url: [dir: 'js/angular/lib/', file: "angular.js"], nominify: true
+    }
+
+    angularResource {
+        dependsOn 'angular'
+        resource id: 'js', url: [dir: 'js/angular/lib/', file: "angular-resource.js"], nominify: true
+    }
+
 }
